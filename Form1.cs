0ad1aa5af94819555e131fd95df9ba85f18bb16b
salome123456 <81592620+salome123456@users.noreply.github.com>
2022-02-27 23:54:01 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "hello world";
+        }
+    }
+}
+/*
+ * 
+ * 1.Which of the following is the correct about class constructor?
+1)a constructor is a special member function of a class that is executed whenever we create new objects or class 
+2)a constructor has the same name as that of a class and it does not have any return type 
+3)both of te above
+
+
+2.Function overloading is a kind of static polymorphism
+1) true 2)false
+
+3.Which of the following converts a floating point or integer type to a decimal type in C#?
+
+1) ToDecimal 2) ToDouble 3)ToInt32
+
+ 4.	
+Which of the following can be facilitated by the Inheritance mechanism?
+ 
+ 1)Use the existing functionality of base class.
+2)Overrride the existing functionality of base class.
+3)Implement new functionality in the derived class.
+4)Implement polymorphic behaviour.
+5)Implement containership.
+
+*input your answer*
+
+
+
+	
+5.How can you prevent inheritance from a class in C#.NET ?
+
+A.	Declare the class as shadows.
+B.	Declare the class as overloads.
+C.	Declare the class as sealed.
+D.	Declare the class as suppress.
+E.	Declare the class as override.
+ 
+ */
